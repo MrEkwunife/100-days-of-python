d550514f3768f54a53f0fa5ed198c792968d8f60
Chimobi Ekwunife <114434873+MrEkwunife@users.noreply.github.com>
2024-01-21 07:10:10 +0000

    almost perfect

--- hangman/final-hangman/main.py
+++ hangman/final-hangman/main.py
@@ -1,13 +1,14 @@
-import hangman_art, hangman_words, os, random
+#!/usr/bin/python3
 
-#Getting random word from list of words
+import hangman_art, hangman_words, os, random, string, time
+
+print(hangman_art.logo)
+
 get_random_word = random.choice(hangman_words.word_list)
 # print(get_random_word)
 
-#List that stores wrong guesses
 wrng_guess = []
 
-#List where right guesse would be stored, filled with "_" for len(get_random_word)
 rght_guess = []
 for i in get_random_word:
     rght_guess += "_"
@@ -15,9 +16,11 @@ for i in get_random_word:
 lives = 6
 game_over = False
 
-
+start_time = time.time()
 while not game_over:
     usr_inp = input("Enter a letter: ").lower()
+    while usr_inp not in string.ascii_lowercase:
+        usr_inp = input("Enter a valid alphabetical character: ")
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -31,6 +34,7 @@ while not game_over:
         lives -= 1
         wrng_guess.append(usr_inp)
 
+    print(hangman_art.logo)
     print("Wrong guess: " + ' '.join(map(str, wrng_guess)))
     print("Right guess: " + ' '.join(map(str, rght_guess)))
     print(hangman_art.stages[lives])
@@ -39,8 +43,9 @@ while not game_over:
         print("You win!🥳")
         game_over = True
     
-    if lives <= 0:
+    if lives <= 0 or time.time() - start_time > 50:
         print("You Lose!🥲")
+        print(f"The word is {get_random_word}")
         game_over = True
 
 
